646c1541860a974eb55d3683d59da3de6ae13fa6
Austin Keiber <adkeiber@gmail.com>
2024-12-10 00:13:52 -0500

    complete linkTag

--- src/main/java/com/adk/markdown/tag/LinkTag.java
+++ src/main/java/com/adk/markdown/tag/LinkTag.java
@@ -2,7 +2,12 @@ package com.adk.markdown.tag;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * This is technically a modified version of the LLink tag... it doesn't allow for any '[', ']', '(' or ')' except for the original
+ */
 public class LinkTag extends BaseTag{
     public LinkTag(){
         this.supportedSubtags.addAll(Arrays.asList(new BaseTag[]{ }));
@@ -14,7 +19,7 @@ public class LinkTag extends BaseTag{
     public int getTextWithAppliedFormat(String entireText) {
         /**
          * We need to check there isn't 2 of the same character next to each other ex: (( or [[ this breaks format
-         * you also can't have a single ] inside of a [] or a ) inside of a (
+         * you also can't have a single ] inside of a [] or a ) inside of a ( TODO
          */
         System.out.println(entireText);
         int indexOfStartText = entireText.indexOf("[");
@@ -26,16 +31,19 @@ public class LinkTag extends BaseTag{
 
             indexOfStartLink = entireText.indexOf("(", indexOfEndText);
 
-            int duplicateSquareIndex = entireText.indexOf("[", indexOfStartText);
-
-            if((duplicateSquareIndex != -1 && duplicateSquareIndex < indexOfStartLink && !entireText.substring(duplicateSquareIndex, indexOfStartLink).contains("]")) )
-                validFormat = false;
+//            int duplicateSquareIndex = entireText.indexOf("[", indexOfStartText);
+//
+//            if((duplicateSquareIndex != -1 && duplicateSquareIndex < indexOfStartLink && !entireText.substring(duplicateSquareIndex, indexOfStartLink).contains("]")) )
+//                validFormat = false;
 
             if(indexOfStartLink != -1 ) {
 
+                if(indexOfEndText+1 != indexOfStartLink)
+                    validFormat = false;
+
                 indexOfEndLink = entireText.indexOf(")", indexOfStartLink);
 
-                duplicateSquareIndex = entireText.indexOf("[", indexOfStartText);
+//                duplicateSquareIndex = entireText.indexOf("[", indexOfStartText);
 
                 if(indexOfEndLink != -1) {
 
@@ -48,13 +56,17 @@ public class LinkTag extends BaseTag{
             }
         }
 
+        boolean duplicateExists = entireText.lastIndexOf("(") != entireText.indexOf("(") || entireText.lastIndexOf(")") != entireText.indexOf(")") ||
+                entireText.lastIndexOf("]") != entireText.indexOf("]") || entireText.lastIndexOf("[") != entireText.indexOf("[");
+        System.out.println("Duplicate: " + duplicateExists);
+
         if(indexOfEndText == -1 || indexOfStartLink == -1 || indexOfEndLink == -1) {
             indexOfEndLink = entireText.length();
             this.validFormat = false;
-        } else if (entireText.substring(indexOfStartText, indexOfEndLink).contains("\n")){
+        } else if (entireText.substring(indexOfStartText, indexOfEndLink).contains("\n") || duplicateExists ){
             validFormat = false;
         }
-
+        System.out.println("Valid format: " + this.validFormat);
         return indexOfEndLink;
     }
 }
